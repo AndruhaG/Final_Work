090170e9d97009bf3a57558aa40594c48caaef41
kozlov andrey <andruha.76-44@mail.ru>
2022-08-09 22:21:32 +0300

    Написали алгоритм решения задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Написать программу, которая из имеющегося массива строк формирует 
+массив из строк, длина которых меньше либо равна 3 символа. 
+Первоначальный массив можно ввести с клавиатуры, либо задать 
+на старте выполнения алгоритма. 
+Примеры:
+["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+["1234", "1567", "-2", "computer science"] -> ["-2"]
+["Russia", "Denmark", "Kazan"] -> []
+*/
+
+
+string[] array1 = new string[] {"123", "ура", "world", "го", "hello", ".,:"};
+Console.WriteLine(); 
+
+for (int i = 0; i < array1.Length; i++) 
+ {
+    if (array1[i].Length <= 3) 
+    Console.Write($"{array1[i]}" + "\t");  
+ }
+ 
+
